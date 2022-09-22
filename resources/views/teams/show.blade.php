a60a34d81c72fd82b80d4db44c92dd589fc2e855
Dmitrii <dm161272@gmail.com>
2022-09-22 12:51:15 +0200

    changed teams and matches management headers

--- resources/views/teams/show.blade.php
+++ resources/views/teams/show.blade.php
@@ -4,11 +4,32 @@
 @include('partials._logo')
 
 <div class="mx-4">
-    <x-slot class="p-10">
+    
+    <div class="flex flex-col items-center justify-center text-center">
+        <img
+            class="hidden w-24 mr-6 md:block"
+           
+            src="{{$team->logo ? asset('storage/' . $team->logo) : asset('/images/noimage.png')}}" alt=""/>
 
-       Content
+        <h3 class="text-2xl mb-2">{{ $team->name }}</h3>
+        <ul class="flex">
+            <li
+                class="bg-sky-500 text-white rounded-xl px-3 py-1 mr-2"
+            >
+                <h2>{{'City: ' . $team->city }}</h2>
+            </li>
+            <li
+                class="bg-sky-500 text-white rounded-xl px-3 py-1 mr-2"
+            >
+                <h2>{{'Country: ' . $team->country }}</h2>
+            </li>
+        </ul>
+            </div>
 
-    </x-slot> 
+        </div>
+      
+        </div>
+    </div>
        
 </div>
 
